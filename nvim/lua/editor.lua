b584c7cac948d772eb08feaa4c290e4f1612d1f2
OnionCall <orion.callaghan@gmail.com>
2024-03-05 00:14:55 -0600

    Initial commit

---
+++ nvim/lua/editor.lua
@@ -0,0 +1,7 @@
+vim.cmd("set softtabstop=0 noexpandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+vim.cmd("set number relativenumber")
+vim.cmd("set nu rnu")
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
