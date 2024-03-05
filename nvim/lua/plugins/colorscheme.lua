9f50fd0a7056f8320645f50ed1377403cc9543a1
OnionCall <orion.callaghan@gmail.com>
2024-03-05 00:36:17 -0600

    bs tweaks

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	"ellisonleao/gruvbox.nvim",
+	priority = 999,
+	config = true,
+	opts = ...,
+	config = function()
+		vim.cmd("colorscheme gruvbox")
+	
+--	"catppuccin/nvim",
+--	name = "catppuccin",
+--	priority = 1000,
+--	config = function()
+--		vim.cmd("colorscheme catppuccin")
+	end
+}
+
